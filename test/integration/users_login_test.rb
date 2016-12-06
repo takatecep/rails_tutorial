2220f87d23a5c9179ea75c6db527b178a2312e2d
takatecep <takatecep@gmail.com>
2016-12-07 03:16:37 +0900

    ログアウト機能追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -15,7 +15,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test 'login with valid information' do
+  test 'login with valid information followed by logout' do
     get login_path
     assert_template 'sessions/new'
     post login_path, params:
@@ -32,5 +32,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', login_path, count: 0
     assert_select 'a[href=?]', logout_path
     assert_select 'a[href=?]', user_path(@user)
+    delete logout_path
+    assert_not logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path, count: 0
+    assert_select 'a[href=?]', user_path(@user), count: 0
   end
 end

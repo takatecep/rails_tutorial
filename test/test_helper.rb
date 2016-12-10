8438b4a5d5426cb7a887272a73c0638642a02ef6
takatecep <takatecep@gmail.com>
2016-12-10 17:37:09 +0900

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,5 +14,21 @@ module ActiveSupport
     def logged_in?
       !session[:user_id].nil?
     end
+
+    # テストユーザーとしてログインする
+    def log_in_as(user)
+      session[:user_id] = user.id
+    end
+  end
+end
+
+module ActionDispatch
+  class IntegrationTest
+    # テストユーザーとしてログインする
+    def log_in_as(user, password: 'password', remember_me: '1')
+      post login_path, params: { session: { email: user.email,
+                                            password: password,
+                                            remember_me: remember_me } }
+    end
   end
 end

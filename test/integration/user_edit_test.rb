ed6c79d1afc93cb9f8e910098aa1b2690b64be63
takatecep <takatecep@gmail.com>
2016-12-10 21:37:48 +0900

    ユーザーアップデート機能追加

---
+++ test/integration/user_edit_test.rb
@@ -0,0 +1,55 @@
+require 'test_helper'
+
+class UserEditTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'unsuccessful edit' do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: {
+      user: {
+        name: '',
+        email: 'foo@invalid',
+        password: '',
+        password_confirmation: ''
+      }
+    }
+    assert_template 'users/edit'
+    assert_select 'div.alert-danger', 'The form contains 2 errors.'
+  end
+
+  test 'successful edit with friendly forwarding' do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    name  = 'Foo Bar'
+    email = 'foo@bar.com'
+    patch user_path(@user), params: {
+      user: {
+        name: name,
+        email: email,
+        password: '',
+        password_confirmation: ''
+      }
+    }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+
+  test 'hoge' do
+    get edit_user_path(@user)
+    assert_redirected_to login_url
+    assert session[:forwarding_url], edit_user_path(@user)
+    log_in_as @user
+    assert session[:forwarding_url].nil?
+    # log_out
+    # log_in_as @user
+    # assert_redirected_to user_url(@user)
+  end
+end

59c9b1c214a9a7b9026dbff3c54bc0bbbc1e6775
takatecep <takatecep@gmail.com>
2016-12-11 15:06:53 +0900

    ユーザー削除機能の追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -45,4 +45,8 @@ module SessionsHelper
   def store_location
     session[:forwarding_url] = request.original_url if request.get?
   end
+
+  def current_user?(user)
+    user == current_user
+  end
 end
